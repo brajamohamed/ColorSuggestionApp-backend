299241f0c591ac32dea074ac03232dfd3d1dc55c
brajamohamed <b.rajamohamed@gmail.com>
2024-02-14 23:38:16 +0530

    added cors

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const dbConnect = require("./dbConnect");
 const userRoute = require("./routes/userRoutes");
 dbConnect();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use(express.json());
 app.use("/user", userRoute);
 
