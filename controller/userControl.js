4034a17ac0cfe67a493a2643212ecbfdf49f3b56
brajamohamed <b.rajamohamed@gmail.com>
2024-02-15 11:41:12 +0530

    fixed status code in userlogin

--- controller/userControl.js
+++ controller/userControl.js
@@ -47,17 +47,21 @@ const getAUser = asyncHandler(async (req, res) => {
 // USER LOGIN
 const userLogin = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
-  const user = await User.findOne({ email: email });
-  if (!user) {
-    return res.status(404).json({ error: "User not found" });
-  }
-  if (await bcrypt.compare(password, user.password)) {
-    const jwtoken = await generateToken(user.id);
-    res
-      .status(500)
-      .json({ message: "User logged in successfully", jwt: jwtoken });
-  } else {
-    res.status(400).json({ error: "Invalid Credentials" });
+  try {
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    if (await bcrypt.compare(password, user.password)) {
+      const jwtoken = await generateToken(user.id);
+      res
+        .status(200)
+        .json({ message: "User logged in successfully", jwt: jwtoken });
+    } else {
+      res.status(400).json({ error: "Invalid Credentials" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "I don't know what happened" });
   }
 });
 // UPDATE USER DETAILS
