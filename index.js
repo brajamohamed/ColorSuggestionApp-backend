b1618694f1eeedeaf08906150e2644eae486689c
brajamohamed <b.rajamohamed@gmail.com>
2024-02-16 11:35:02 +0530

    upadated cors middleware

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const dotenv = require("dotenv").config();
 const dbConnect = require("./dbConnect");
 const userRoute = require("./routes/userRoutes");
 dbConnect();
-app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    headers: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 app.use("/user", userRoute);
